0bb9e9484b1a232305a3159e42b63dcda5393a19
Daniil Uriev <89259760+danicbro12@users.noreply.github.com>
2022-06-22 18:17:21 +0300

    eng version

--- presence_programm.py
+++ presence_programm.py
@@ -3,28 +3,28 @@ from pypresence import Presence
 import time
 
 while True:
-    client_id = int(input('Айди приложения в ddp: '))  # айди приложения в ddp
+    client_id = int(input('Application id in ddp: '))  # application id in ddp
 
     if client_id == None:
-        print('Введите айди приложения в ddp')  # проверка на ввод айди
+        print('Enter application id in ddp')  # check for input id
         exit()
     else:
-        RPC = Presence(client_id, pipe=0)  # хуйня какая-то
-        RPC.connect()  # подключаемся к discord
-        details = input('Описание вашей активности: ')  # текст присутствия
+        RPC = Presence(client_id, pipe=0)  # some bullshit
+        RPC.connect()  # connect to discord
+        details = input('Description of your activity: ')  # presence text
         if details == '':
-            print('Описание обязательно')
+            print('Description required')
         else:
-            state = input('Что вы конкретно делаете: ')  #
+            state = input('What exactly are you doing: ')  #
             if state == '':
-                large_image = input('Ссылка на изображение: ')  # ссылка на изображение
+                large_image = input('Image link: ')  # image link
                 if large_image == '':
-                    while True:  # бесконечный цикл
+                    while True:  # infinite loop
                         print(RPC.update(details=details))
-                        # обновляем присутствие
-                        time.sleep(15)  # пауза в 15 секунд
+                        # update presence
+                        time.sleep(15)  # pause for 15 seconds
                 else:
-                    large_text = input('Текст изображения: ')  # текст изображения
+                    large_text = input('Image text: ')  # image text
                     if large_text is None:
                         print(RPC.update(details=details,
                                          large_image=large_image))
@@ -33,15 +33,15 @@ while True:
                                          large_image=large_image,
                                          large_text=large_text))
             else:
-                large_image = input('Ссылка на изображение: ')  # ссылка на изображение
+                large_image = input('Image link: ')  # image link
                 if large_image == '':
-                    while True:  # бесконечный цикл
+                    while True:  # infinite loop
                         print(RPC.update(details=details,
                                          state=state))
-                        # обновляем присутствие
-                        time.sleep(15)  # пауза в 15 секунд
+                        # update presence
+                        time.sleep(15)  # pause for 15 seconds
                 else:
-                    large_text = input('Текст изображения: ')  # текст изображения
+                    large_text = input('Image text: ')  # image text
                     if large_text is None:
                         print(RPC.update(details=details,
                                          state=state,
@@ -50,3 +50,4 @@ while True:
                         print(RPC.update(details=details,
                                          large_image=large_image,
                                          large_text=large_text))
+
